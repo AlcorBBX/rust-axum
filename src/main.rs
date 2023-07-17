1cee6a9f031a7e0377a406ccf0ad1065fc8964d9
AlcorBBX <lev.matisov55@gmail.com>
2023-07-17 18:37:04 +0500

    feat(*): query params

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 #![allow(unused)]
 
-use axum::response::Html;
+use axum::extract::Query;
+use axum::response::{Html, IntoResponse};
 use axum::routing::get;
 use axum::Router;
+use serde::Deserialize;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/",
-        get(|| async { Html("Hello <strong>world!</strong>")})
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
@@ -18,4 +17,17 @@ async fn main() {
         .serve(routes_hello.into_make_service())
         .await
         .unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+
+    Html(format!("Hello <strong>{name}!</strong>"));
 }
